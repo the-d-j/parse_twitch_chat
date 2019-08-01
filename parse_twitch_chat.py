c2296de79976c878a4306a24b7ce1bb5bacd0b7d
DonnieU <thedonnieutley@gmail.com>
2019-07-31 23:22:47 -0500

    responses were working...then suddenly broke. twitch or vm issue?

--- parse_twitch_chat.py
+++ parse_twitch_chat.py
@@ -1,7 +1,40 @@
-import local_settings.py
+from local_settings import NICKNAME, TOKEN, CHANNEL
+import socket
+import re
 
 server = 'irc.chat.twitch.tv'
 port = 6667
-nickname = os.environ.get('NICKNAME')
-token = os.environ.get('TWITCH_TOKEN')
-channel = '#biff_waverider'
+nickname = NICKNAME
+token = TOKEN
+channel = CHANNEL
+
+sock = socket.socket()
+sock.connect((server, port))
+
+sock.send(f"PASS {token}\n".encode('utf-8'))
+sock.send(f"NICK {nickname}\n".encode('utf-8'))
+sock.send(f"JOIN {channel}\n".encode('utf-8'))
+
+
+while True:
+  resp = sock.recv(2048).decode('utf-8')
+
+  if resp.startswith('PING'):
+    sock.send("PONG\n".encode('utf-8'))
+    print('sent PONG')
+  
+  # elif resp.startswith(':tmi.twitch.tv'):
+  #   print('caught :tmi.twitch.tv')
+  
+
+  elif len(resp) > 0:
+    print('response:\n', resp)
+    # username, channel, message = re.search(':(.*)\!.*@.*\.tmi\.twitch\.tv PRIVMSG #(.*) :(.*)', resp).groups()
+    # username, message = re.split('[:]', resp)
+    # print(f"Username: {username}\t Message: {message}\n")
+    # res = re.split('[:]', resp)
+    # print(res)
+
+  resp = None
+
+# sock.close()
